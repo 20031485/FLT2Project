0f64b348c66ebde0efc43a389f1ee11134f45a87
Stefano Alcamo <stefanoalcamo1@gmail.com>
2020-09-16 10:11:09 +0200

    error Handling
    aggiunta della gestione degli errori

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -17,108 +17,99 @@ public class Parser {
 	
 	
 	
-	public void parse() throws SyntaxException {
-		try{
-			parsePrg();
-		}
-		catch(SyntaxException e) {
-			throw new SyntaxException(e.getMessage());
-		}
+	public void parse() throws SyntaxException, IOException, LexicalException {
+		parsePrg();
 	}
 	
 	//Prg -> DSs $
-	void parsePrg() throws SyntaxException{
-		try{
-			Token token = scanner.peekToken();
-			//print("parsePrg");
-			switch(token.getType()) {
-				case FLOATDEC:
-				case INTDEC:
-				case ID:
-				case PRINT:
-				case EOF:
-					parseDSs();
-					match(TokenType.EOF);
-					return;
-				default:
-					throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token: "+token.toString());
-			}
-		}
-		catch(IOException | LexicalException | SyntaxException e) {
-			throw new SyntaxException(e.getMessage());
+	void parsePrg() throws SyntaxException, IOException, LexicalException {
+		Token token = scanner.peekToken();
+		Token wrongToken = null;
+		//print("parsePrg");
+		switch(token.getType()) {
+			case FLOATDEC:
+			case INTDEC:
+			case ID:
+			case PRINT:
+			case EOF:
+				parseDSs();
+				match(TokenType.EOF);
+				return;
+			default:
+				wrongToken = token;
+				token = scanner.nextToken();
+				parsePrg();
+				throw new SyntaxException("SyntaxException@line:"+wrongToken.getLine()+"\nUnexpected token!");
 		}
 	}
 	
 	//DSs -> Dcl DSs | Stm DSs | eps
-	private void parseDSs() throws SyntaxException {
-		try {
-			Token token = scanner.peekToken();
-			//print("parseDSs");
-			switch(token.getType()) {
-				case FLOATDEC:
-				case INTDEC:
-					parseDcl();
-					parseDSs();
-					return;
-				case ID:
-				case PRINT:
-					parseStm();
-					parseDSs();
-					return;
-				case EOF:
-					//match(TokenType.EOF);//--> ritorno soltanto, il match di EOF è in parsePrg!
-					return;
-				default:
-					throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token: "+token.toString());
-			}
-		}
-		catch(IOException | LexicalException | SyntaxException e) {
-			throw new SyntaxException(e.getMessage());
+	private void parseDSs() throws IOException, LexicalException, SyntaxException {
+		Token token = scanner.peekToken();
+		Token wrongToken = null;
+		//print("parseDSs");
+		switch(token.getType()) {
+			case FLOATDEC:
+			case INTDEC:
+				parseDcl();
+				parseDSs();
+				return;
+			case ID:
+			case PRINT:
+				parseStm();
+				parseDSs();
+				return;
+			case EOF:
+				//match(TokenType.EOF);//--> ritorno soltanto, il match di EOF è in parsePrg!
+				return;
+			default:
+				wrongToken = token;
+				token = scanner.nextToken();
+				parsePrg();
+				throw new SyntaxException("SyntaxException@line:"+wrongToken.getLine()+"\nUnexpected token!");
 		}
 	}
 	
 	//Dcl -> tyFloat id; | tyInt id;
-	private void parseDcl() throws SyntaxException {
-		try{
-			Token token = scanner.peekToken();
-			//print("parseDcl");	
-			switch(token.getType()) {
-				case FLOATDEC:
-					match(TokenType.FLOATDEC);
-					match(TokenType.ID);
-					match(TokenType.SEMI);
-					return;
-				case INTDEC:
-					match(TokenType.INTDEC);
-					match(TokenType.ID);
-					match(TokenType.SEMI);
-					return;
-				default:
-					throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token: "+token.toString());
-			}
-		}
-		catch(IOException | LexicalException | SyntaxException e) {
-			throw new SyntaxException(e.getMessage());
+	private void parseDcl() throws IOException, LexicalException, SyntaxException {
+		Token token = scanner.peekToken();
+		Token wrongToken = null;
+		//print("parseDcl");
+		switch(token.getType()) {
+			case FLOATDEC:
+				match(TokenType.FLOATDEC);
+				match(TokenType.ID);
+				match(TokenType.SEMI);
+				return;
+			case INTDEC:
+				match(TokenType.INTDEC);
+				match(TokenType.ID);
+				match(TokenType.SEMI);
+				return;
+			default:
+				wrongToken = token;
+				token = scanner.nextToken();
+				parsePrg();
+				throw new SyntaxException("SyntaxException@line:"+wrongToken.getLine()+"\nUnexpected token!");
 		}
 	}
 	
 	//Stm -> print id; | id = Exp;
-	private void parseStm() throws SyntaxException {
-		try {
-			Token token = scanner.peekToken();
-			//print("parseStm");
-			switch(token.getType()) {
-				case PRINT:
-					match(TokenType.PRINT);
-					match(TokenType.ID);
-					match(TokenType.SEMI);
-					return;
-				default:
-					throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token: "+token.toString());
-			}
-		}
-		catch(IOException | LexicalException | SyntaxException e) {
-			throw new SyntaxException(e.getMessage());
+	private void parseStm() throws IOException, LexicalException, SyntaxException {
+		Token token = scanner.peekToken();
+		Token wrongToken = null;
+		//print("parseStm");
+		switch(token.getType()) {
+			case PRINT:
+				match(TokenType.PRINT);
+				match(TokenType.ID);
+				match(TokenType.SEMI);
+				return;
+			default:
+				wrongToken = token;
+				token = scanner.nextToken();
+				parsePrg();
+				throw new SyntaxException("SyntaxException@line:"+wrongToken.getLine()+"\nUnexpected token!");
 		}
 	}
 	
@@ -129,7 +120,7 @@ public class Parser {
 			print(nextToken.toString());
 		}
 		else
-			throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token: "+token.toString());
+			throw new SyntaxException("SyntaxException@line:"+token.getLine()+"\nUnexpected token!");
 	}
 	
 	public void print(String s) {
